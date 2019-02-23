2bd5525746ea99cf73cbd62fd6b341645d0156e1
Thomas <thomas@craftacademy.se>
2019-02-23 11:57:26 +0100

    initial commit after cleaning up scaffold adds Hello component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import Hello from './Hello'
+import './index.css'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      greetingSubject: "Faraz"
+    }
+  }
+
+  changeTheName() {
+    const names = ['Thomas', 'Faraz', 'Noel', 'Adi']
+    var rand = names[Math.floor(Math.random() * names.length)];
+    this.setState({greetingSubject: rand})
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Hello name={this.state.greetingSubject} />
+        <button onClick={this.changeTheName.bind(this)}>
+          Change name
+        </button>
       </div>
-    );
+    )
   }
 }
+export default App
 
-export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <h1>
+        Hello {props.name}
+      </h1>
+    </div>
+  )
+}
+export default Hello
+
